d0074c177a49cbe60e72282bc71722ca60707fbf
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-28 01:38:45 -0500

    Añadiendo Fonts layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,25 @@
 import Navbar from "@/components/Navbar";
+import { Roboto } from "next/font/google";
+
 export const metadata = {
     title: "Post con nextJS",
     description: "",
-    keywords:"NextJs - React"
+    keywords: "NextJs - React",
 };
+
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "700"],
+    styles: ["normal", "italic"],
+    variable: "--font-roboto",
+});
 
 // These styles apply to every route in the application
 import "./globals.css";
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="es">
+        <html lang="es" className={`${roboto.className}`}>
             <body>
                 <Navbar />
                 {children}
